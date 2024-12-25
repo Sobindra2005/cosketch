c57de14860006f3e996abe0ea951c2925c7c16c3
sobindra2005 <sobhindra2005@gmail.com>
2024-12-25 19:28:58 +0545

    Enhance participant status display with colored indicators

--- src/components/participants.tsx
+++ src/components/participants.tsx
@@ -19,7 +19,7 @@ const Participants: React.FC<ParticipantsProps> = (props) => {
                 { name: "Sugriva", status: "inactive" }].map((color, index) => (
                     <div key={index} className='flex  w-full h-14 justify-between p-2  items-center'><div className='
                     flex h-12  p-1'><img src='https://i.pinimg.com/originals/f1/0f/f7/f10ff70a7155e5ab666bcdd1b45b726d.jpg' className=' object-cover object-center h-full rounded-full mr-5 ' alt='null'></img> <div className=' text-[16px] flex items-center '>{color.name} </div>  </div>
-                        <div className={`items-center  text-sm ${color.status === "active" ? "text-yellow-400":"text-red-600"}`} >{color.status}</div>
+                        <div className={`items-center  text-sm ${color.status === "active" ? "text-yellow-400":"text-red-600"}`} >{color.status === "active"?<span className='w-3 h-3 bg-yellow-300 p-1 rounded-full flex items-center '></span>:<span className='w-3 h-3 bg-red-600 p-1 rounded-full flex items-center '></span>}</div>
                     </div>
                 ))}
 
