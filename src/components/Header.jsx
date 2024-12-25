3c5a44651b59bc9ffefa9b2edf0f801ec18cd1ba
sobindra2005 <sobhindra2005@gmail.com>
2024-12-25 19:34:17 +0545

    Add navigation link to header and enhance color component interactivity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Maincomponent from './Maincomponent';
+import Link from 'next/link';
 
 const Header = () => {
     return (
         <Maincomponent className='w-full'>
             <div className='text-xl lg:text-2xl font-bold'>
-                <span className='text-blue-600'>Co</span><span className='text-green-600'>sketch</span>
+                <Link href={'/'} >   <span className='text-blue-600'>Co</span><span className='text-green-600'>sketch</span></Link>
             </div>
         </Maincomponent>
     );
--- src/components/color.tsx
+++ src/components/color.tsx
@@ -5,13 +5,13 @@ import { IoAddOutline } from 'react-icons/io5';
 const Color = () => {
     return (
         <MainComponent className=' max-w-2xl h-fit mt-2'>
-            <div className='flex gap-1 text-xl'>
+            <div className='flex gap-1 text-xl '>
                 {["#000000", "#FF0000", "#06F906", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF"].map((color, index) => (
-                    <div key={index} className='p-1 cursor-pointer rounded-lg  w-10 h-8'>
+                    <div key={index} className='p-1 hover:border hover:border-gray-300  cursor-pointer rounded-lg  w-10 h-8'>
                         <div className={`w-full h-full rounded-3xl `} style={{ backgroundColor: color }}></div>
                     </div>
                 ))}
-                <div className='p-1 cursor-default rounded-lg  w-10 h-8'>
+                <div className='p-1 hover:border hover:border-gray-300  cursor-default rounded-lg  w-10 h-8'>
                     <div className='text-gray-400 text-2xl cursor-pointer rounded-3xl border border-gray-400 flex items-center justify-center w-full h-full '><IoAddOutline /></div>
                 </div>
             </div>
